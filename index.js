6ea3ab6024f316c8766b49df55d0e3aebfde620b
Joe Chen <support@aristai.io>
2026-01-09 11:32:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello from the service!\n');
+    res.end('Hello from the serviceafjhsklajdlslka;djsalkdsa!\n');
 });
 
 server.listen(port, () => {
