25c1f4e1845d69878891f6ae02c7bd3ae7d8d92b
Joe Chen <support@aristai.io>
2026-01-09 11:22:25 -0600

    add

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-hahahaahassss
+const http = require('http');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello from the service!\n');
+});
+
+server.listen(port, () => {
+    console.log(`Server running at http://0.0.0.0:${port}/`);
+});
